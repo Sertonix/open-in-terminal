414c1da10dcd34347d06a94b12b8468305fe3687
Sertonix <sert0nix@protonmail.com>
2022-10-26 20:20:08 +0200

    init repository

---
+++ index.js
@@ -0,0 +1,100 @@
+const {Disposable, CompositeDisposable} = require('atom');
+const {spawn} = require('child_process');
+const Path = require('path');
+
+const terminals = {
+    darwin: () => ({
+        default: "terminal",
+        terminals: {
+        },
+    }),
+    linux: () => ({
+        default: "terminal",
+        terminals: {
+        },
+    }),
+    win32: () => ({
+        default: "cmd",
+        terminals: {
+            cmd: {
+                command: `start ${process.env.comspec || Path.join(process.env.SystemRoot || 'C:/windows','system32/cmd.exe')}`,
+                shell: true
+            },
+        },
+    }),
+}[process.platform]?.() || {};
+
+let disposables, treeView;
+
+function openTerminal(cwd) {
+    cwd = Path.resolve(cwd);
+    const [cmd,...args] = `start ${process.env.comspec || Path.join(process.env.SystemRoot || 'C:/windows','system32/cmd.exe')}`.split(' '); // TODO handle quotes https://github.com/mccormicka/string-argv
+    spawn(cmd, args, {cwd,shell:true});
+}
+
+function openFromTreeView() {
+    const path = treeView?.selectedPaths()[0];
+    if (!path) return;
+    openTerminal(path);
+}
+
+function openFromTextEditor() {
+    const textEditor = atom.workspace.getActiveTextEditor();
+    if (!textEditor || textEditor.isRemote /* teletype compatible */) return;
+    const path = textEditor.getPath();
+    if (!path) return;
+    openTerminal(Path.dirname(path));
+}
+
+function activate() {
+    disposables = new CompositeDisposable();
+    disposables.add(
+        atom.commands.add('atom-workspace', {
+            'open-in-terminal:open-selected-folders': openFromTreeView,
+        }),
+        atom.commands.add('atom-workspace', {
+            'open-in-terminal:open-text-editor': openFromTextEditor,
+        }),
+        atom.contextMenu.add({
+            '.tree-view li.directory': [{
+                label: 'Open in Terminal',
+                command: 'open-in-terminal:open-selected-folders'
+            }],
+            '.tab.texteditor': [{
+                label: 'Open in Terminal',
+                command: 'open-in-terminal:open-text-editor'
+            }],
+        }),
+    );
+}
+
+function deactivate() {
+    disposables?.dispose();
+    disposables = undefined;
+}
+
+module.exports = {
+    activate,
+    deactivate,
+    consumeTreeView: tv => treeView = tv,
+    config: {
+        command: {
+			title: "Start command",
+			description: "The command used to spawn the terminal",
+			type: "string",
+			default: {
+                linux: () => "/usr/bin/gnome-terminal --working-directory {cwd}",
+                win32: () => `start ${process.env.comspec || Path.join(process.env.SystemRoot || 'C:/windows','system32/cmd.exe')}`,
+                darwin: () => "open -b com.apple.Terminal {cwd}",
+            }[process.platform]?.() || '',
+            order: 1,
+		},
+		useShell: {
+			title: "Use shell",
+			description: "Enable if `spawn` ",
+			type: "boolean",
+			default: process.platform === "win32",
+            order: 2,
+		},
+    }
+};
